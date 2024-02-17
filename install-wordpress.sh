adb782045ff4e4990b1821e5d768ed61d5d72b51
Azeez Salu <51140317+azeezsalu@users.noreply.github.com>
2024-02-17 00:54:30 -0500

    Add files via upload

---
+++ install-wordpress.sh
@@ -0,0 +1,74 @@
+EFS_DNS_NAME=fs-08b3a985f6783ae09.efs.us-east-1.amazonaws.com
+
+# create to root user
+sudo su
+
+# update the sofware packages on the ec2 instance 
+yum update -y
+
+# create an html directory 
+mkdir -p /var/www/html
+
+# mount the efs to the html directory 
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport "$EFS_DNS_NAME":/ /var/www/html
+
+# install the apache web server, enable it to start on boot, and then start the server immediately
+sudo yum install -y httpd
+sudo systemctl enable httpd 
+sudo systemctl start httpd
+
+# install php 8 along with several necessary extensions for wordpress to run
+sudo dnf install -y \
+php \
+php-cli \
+php-cgi \
+php-curl \
+php-mbstring \
+php-gd \
+php-mysqlnd \
+php-gettext \
+php-json \
+php-xml \
+php-fpm \
+php-intl \
+php-zip \
+php-bcmath \
+php-ctype \
+php-fileinfo \
+php-openssl \
+php-pdo \
+php-tokenizer
+
+# install the mysql version 8 community repository
+sudo wget https://dev.mysql.com/get/mysql80-community-release-el9-1.noarch.rpm 
+#
+# install the mysql server
+sudo dnf install -y mysql80-community-release-el9-1.noarch.rpm 
+sudo rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2023
+dnf repolist enabled | grep "mysql.*-community.*"
+sudo dnf install -y mysql-community-server 
+#
+# start and enable the mysql server
+sudo systemctl start mysqld
+sudo systemctl enable mysqld
+
+# set permissions
+sudo usermod -a -G apache ec2-user
+sudo chown -R ec2-user:apache /var/www
+sudo chmod 2775 /var/www && find /var/www -type d -exec sudo chmod 2775 {} \;
+sudo find /var/www -type f -exec sudo chmod 0664 {} \;
+chown apache:apache -R /var/www/html 
+
+# download wordpress files
+wget https://wordpress.org/latest.tar.gz
+tar -xzf latest.tar.gz
+cp -r wordpress/* /var/www/html/
+
+# create the wp-config.php file
+cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+
+# edit the wp-config.php file
+vi /var/www/html/wp-config.php
+
+# restart the webserver
+service httpd restart
